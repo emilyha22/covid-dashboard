89e5f04e253ee9c4f019990e5fa63b3bdc7469f4
Brian Daley <382814+bdaley@users.noreply.github.com>
2021-02-18 13:26:25 -0500

    Bugfix/error 13 (#6)
    * trying with  set to true
    
    * no more streaming
    
    * trying batch writes
    
    * adjustments
    
    * going another route
    
    * file copy works

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,38 @@
 require('dotenv').config()
 const functions = require('firebase-functions');
-const parse = require('csv-parse');
+const parse = require('csv-parse/lib/sync');
 const got = require('got');
 
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+// Firestore config
+// @url https://firebase.google.com/docs/reference/js/firebase.firestore.Settings
+const db = admin.firestore();
+db.settings({
+    ignoreUndefinedProperties: true
+})
+
+const bucket = admin.storage().bucket();
+
+/**
+ * Each CSV that we'll import
+ */
+const csvImports = [
+    {
+        id: 'imported',
+        description: 'This holds the Storrs campus data',
+        url: `https://${process.env.GITHUB_USER}:${process.env.GITHUB_TOKEN}@${process.env.RAW_WASTEWATER_URL}`,
+        ref: 'samples/storrs.csv'
+    },
+    {
+        id: 'windham',
+        description: 'This holds the town of Windham data',
+        url: `https://${process.env.GITHUB_USER}:${process.env.GITHUB_TOKEN}@${process.env.WINDHAM_WASTEWATER_URL}`,
+        ref: 'samples/windham.csv'
+    },
+    
+]   
 
 const mappings = {
     "HV"    : "QuarantineDorm",
@@ -24,136 +52,123 @@ const mappings = {
     "GRAD"  : "IsolationDorm2"
 }
 
-/**
- * TODO: Unit Tests
- */
+
 exports.importWastewaterData = functions.https.onRequest( async (req, res) => {
 
+    // Only accept post requests
     if(req.method !== 'POST') return res.status(403).send("Forbidden");
 
-    // Wrap our work in a promise to control when we send the http response
-    const allImportsFinished = new Promise( (resolve, reject) => {
 
-        /**
-         * Each CSV that we'll import
-         */
-        const csvImports = [
-            {
-                id: 'imported',
-                description: 'This holds the Storrs campus data',
-                url: `https://${process.env.GITHUB_USER}:${process.env.GITHUB_TOKEN}@${process.env.RAW_WASTEWATER_URL}`
-            },
-            {
-                id: 'windham',
-                description: 'This holds the town of Windham data',
-                url: `https://${process.env.GITHUB_USER}:${process.env.GITHUB_TOKEN}@${process.env.WINDHAM_WASTEWATER_URL}`
-            },
-            
-        ]    
+    let saveStatus = new Promise( (resolve, reject) => {
+        csvImports.forEach( (csv, index, arr) => {
 
-        // Human readable ID for our import job
-        const importID = Date();    
-
-        // Root Collection for all sample imports
-        const rootCollection = process.env.NODE_ENV == 'production' ? 'samples' : 'samplesUnitTests';
-        const rootCollectionRef = admin.firestore().collection(rootCollection);    
-
-        //  Storage for all of the "write" promises.
-        let writes = {};
-
-        // Counter for the number of imported CSV files
-        let csvCounter = 0;
-
-        /**
-         * Import each CSV files
-         */
-        csvImports.forEach( async (csv) => {
-
-            // The collection for the current import
-            let collectionRef = rootCollectionRef.doc( csv.id ).collection( importID );
-
-            // Where we reference the current sample set. Serves as a pointer later on.
-            // let currentSampleSetDoc = rootCollectionRef.doc('currentSampleSet');        
-
-            // Most recent collection date
-            let mostRecentCollectionDate = "";
-
-            // Namespace the "writes" by the csv id, to keep them separate from other writes
-            writes[csv.id] = [];
-
-            /**
-             * Read and parse the CSV stream
-             * 
-             * @url https://csv.js.org/parse/api/stream/
-             */
-            let parser = got(csv.url, { isStream: true }).pipe(parse({
-                columns: true,
-                trim: true       
-            }));
-
-            // Iterate over each record/row in the CSV
-            for await (const record of parser) {
-                
-                // use empty column name as "id"
-                let id = record[''];
-                delete record[''];
-
-                // Rename locations in the Storrs csv only
-                if(record['location']){
-                    record['location'] = mappings[record['location']];
-                }
-                
-
-                if (record.date.replace('-', '') > mostRecentCollectionDate.replace('-', '') && record.date !== 'NA'){
-                    mostRecentCollectionDate = record.date;
-                }
-
-                /**
-                 * I would batch these, but there's a limit of
-                 * 500 records, so we'd have to split everything into
-                 * separate batches. Future me.
-                 * 
-                 * TODO:  Split writes into batches
-                 */
-                writes[csv.id].push(collectionRef.doc(id).set(record, {merge: true}));           
-            }
-            
-            // Handle the success or failure of our writes
-            Promise.all(writes[csv.id]).then( async (values) => {
-                /**
-                 * Success! Update the reference to the current data set
-                 * so everyone can find it among our collections
-                 */ 
-                await rootCollectionRef.doc( csv.id ).set({
-                    "currentSampleSet": {
-                        "collectionID": importID,
-                        "mostRecentCollectionDate": mostRecentCollectionDate
+            // Get CSV Data
+            got(csv.url).then( (response) => {
+                return response.body;
+            }).then(csvData => {
+                // Save the csv file
+                bucket.file(csv.ref).save(csvData).then(()=>{
+                    if(index+1 === arr.length){
+                        // if this was the last file, we resolve the promise
+                        resolve('Samples were imported successfully.')
                     }
-                }, {merge: true})
-                functions.logger.log(`${writes[csv.id].length} samples were imported.`);
-                csvCounter++;
-                
-                // Again, this feels dumb, but..
-                if(csvCounter === csvImports.length){
-                    resolve()
-                }
+                }).catch(error => {
+                    reject(error)
+                })
             }).catch(error => {
-                // Fahhk - something went wrong
-                functions.logger.log(error);
-                reject()
+                reject(error);
             });
-
         });
-
+    })
+ 
+    saveStatus.then((msg)=> {
+        return res.status(200).send(msg);
+    }).catch( error => {
+        return res.status(404).send(error);
     });
 
 
-    /**
-     * There's probably a better way to do this but...
-     */
-    allImportsFinished.then(val => {
-        res.send("Samples were imported successfully.");
-    }).catch(error => {
-        res.send("Samples were not imported successfully.");
-    });
+
+
+    // // Human readable ID for our import job
+    // const importID = Date();    
+
+    // // Root Collection for all sample imports
+    // const rootCollection = process.env.NODE_ENV === 'production' ? 'samples' : 'samplesUnitTests';
+    // const rootCollectionRef = admin.firestore().collection(rootCollection); 
+
+    // /**
+    //  * Import each CSV files
+    //  */
+    // csvImports.forEach( async (csv) => {
+
+    //     // The collection for the current import
+    //     let collectionRef = rootCollectionRef.doc( csv.id ).collection( importID );      
+
+    //     // Most recent collection date
+    //     let mostRecentCollectionDate = "";
+
+
+    //     /**
+    //      * Read and parse the CSV
+    //      * 
+    //      * @url https://csv.js.org/parse/api/sync/
+    //      */
+    //     let csvData;
+    //     try {
+    //         let { body } = await got(csv.url);
+    //         csvData = body;
+    //     } catch (error) {
+    //         return res.send(`Couldn't fetch CSV file: ${error}`)            
+    //     }
+    //     const records = parse(csvData, {
+    //         columns: true,
+    //         skip_empty_lines: true,
+    //         trim: true
+    //     })
+
+    //     let chunks = chunkArray(records, 500);
+
+    //     chunks.forEach( async (chunk, index) => {
+
+    //         let batches = [];
+    //         batches[index] = db.batch();
+
+    //         // Iterate over each record/row in the CSV
+    //         chunk.forEach( async (record) => {
+
+                
+    //             // use empty column name as "id"
+    //             let id = record[''];
+    //             delete record[''];
+
+    //             // Rename locations in the Storrs csv only
+    //             if(record['location']){
+    //                 record['location'] = mappings[record['location']];
+    //             }
+                
+
+    //             if (record.date.replace('-', '') > mostRecentCollectionDate.replace('-', '') && record.date !== 'NA'){
+    //                 mostRecentCollectionDate = record.date;
+    //             }
+
+    //             batches[index].set(collectionRef.doc(id), record, {merge: true})
+
+
+    //         });
+
+    //         await batches[index].commit();
+
+    //     });
+
+    //     await rootCollectionRef.doc( csv.id ).set({
+    //         "currentSampleSet": {
+    //             "collectionID": importID,
+    //             "mostRecentCollectionDate": mostRecentCollectionDate
+    //         }
+    //     }, {merge: true})         
+        
+
+    // });
+
 });
